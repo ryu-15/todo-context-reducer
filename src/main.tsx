31c89ecf58c1d9c5db52848fdab7d8509dfb9f41
ryu-15 <meghagrg15@gmail.com>
2025-01-27 16:44:44 +0545

    feat[ui]: apply theme class to body

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client';
 
 import '@/styles/globalStyle/index.css.ts';
 import App from '@/views/App.tsx';
+import themeData from '@/styles/theme/themeData/themeData.css.ts';
 
+document.body.className = themeData;
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
