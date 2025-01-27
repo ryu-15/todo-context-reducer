1ae76065e0be6019086034d937f9d0e1bb8814e2
ryu-15 <meghagrg15@gmail.com>
2025-01-27 16:59:34 +0545

    feat(todo): implement todo status update and user-specific reset

--- src/providers/TodoProvider/index.tsx
+++ src/providers/TodoProvider/index.tsx
@@ -1,14 +1,17 @@
 import React, { PropsWithChildren, useEffect, useReducer } from 'react';
 
-import { ITodo, TodoAction, ITodoState } from '@/data/model';
+import { ITodo, TodoAction, ITodoState } from '@/data/model/TodoModel';
 import { TodoContext } from '@/context/TodoContext';
-import { STORAGE_TYPE, TODO_ACTION_TYPE } from '@/data/constant';
+import { STORAGE_TYPE, TODO_ACTION_TYPE } from '@/data/constant/TodoConstant';
+import useAuthData from '@/utils/useAuthData.ts';
 
 const TodoProvider: React.FunctionComponent<PropsWithChildren> = ({ children }) => {
   const getInitialTodos = (storageType: STORAGE_TYPE): ITodo[] => {
     const storage = storageType === STORAGE_TYPE.LOCAL ? localStorage : sessionStorage;
     return JSON.parse(storage.getItem('todos') || '[]');
   };
+  const { localUser } = useAuthData();
+  const currentUserId = Number(localUser?.id);
 
   const initialState: ITodoState = {
     todos: getInitialTodos(STORAGE_TYPE.LOCAL),
@@ -22,6 +25,9 @@ const TodoProvider: React.FunctionComponent<PropsWithChildren> = ({ children }) 
           id: action.payload.id,
           task: action.payload.task,
           status: action.payload.status,
+          userId: action.payload.userId,
+          starting_date: new Date(),
+          ending_date: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
         };
         return {
           ...state,
@@ -41,6 +47,13 @@ const TodoProvider: React.FunctionComponent<PropsWithChildren> = ({ children }) 
             todo.id === action.payload.id ? { ...todo, task: action.payload.task } : todo,
           ),
         };
+        case TODO_ACTION_TYPE.UPDATE_TODO_STATUS:
+        return {
+          ...state,
+          todos: state.todos.map(todo =>
+            todo.id === action.payload.id ? { ...todo, status: action.payload.status } : todo,
+          ),
+        };
 
       case TODO_ACTION_TYPE.SET_STORAGE_TYPE:
         return {
@@ -50,10 +63,8 @@ const TodoProvider: React.FunctionComponent<PropsWithChildren> = ({ children }) 
         };
 
       case TODO_ACTION_TYPE.RESET_TODOS:
-        return {
-          ...state,
-          todos: []
-        };
+        { const remainingTodos = state.todos.filter(todo => todo.userId !== currentUserId);
+        return { ...state, todos: remainingTodos }; }
 
       default:
         return state;
